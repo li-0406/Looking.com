a1d305f4ab1c638def7ca467549be180bd3cee15
10320賴慕宇 <cr870406@gmail.com>
2024-01-10 10:19:46 +0800

    訂房彈窗調整

--- src/components/Reservationbtn.jsx
+++ src/components/Reservationbtn.jsx
@@ -1,11 +1,56 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 import useFetch from "../hooks/useFetch";
+import { OptionsContext } from "./context/OptionsContext.js";
+import { LoginContext } from "./context/LoginContext.js";
+import axios from "axios";
+import useCreateOrder from "../hooks/useCreateOrder.js";
 
 const Reservationbtn = ({ onClose, id }) => {
   const { data, loading, error } = useFetch(`/rooms/findHotel/${id}`);
-  console.log(data);
+
+  const { date, options } = useContext(OptionsContext);
+  const { user } = useContext(LoginContext);
+
+  const [roomNumber, setRoomNumber] = useState([]);
+  const [orderData, setOrderData] = useState({
+    userID: user._id,
+    hotelId: id,
+    RoomNumberId: [],
+    ReservationDates: [
+      {
+        startDate: date[0].startDate,
+        endDate: date[0].endDate,
+      },
+    ],
+    totalPrice: 1,
+    options: {
+      adult: options.adult,
+      children: options.children,
+      rooms: options.room,
+    },
+  });
+
+  const handleCheckBox = (e) => {
+    setRoomNumber(
+      e.target.checked
+        ? [...roomNumber, e.target.value]
+        : roomNumber.filter((i) => i !== e.target.value)
+    );
+  };
+
+  const [createOrderState, setCreateOrderState] = useState(false);
+  const { order } = useCreateOrder("/order", orderData, createOrderState);
+  const handleClick = async () => {
+    try {
+      await setOrderData((item) => ({ ...item, RoomNumberId: roomNumber }));
+      setCreateOrderState(true);
+      console.log(order);
+    } catch (error) {
+      console.log("訂單或是住宿日期上傳失敗");
+    }
+  };
   return (
     <div className="fixed w-full h-screen z-50 bg-black/50 flex justify-center items-center">
       <div className="container max-w-screen-lg">
@@ -27,8 +72,8 @@ const Reservationbtn = ({ onClose, id }) => {
               </tr>
             </thead>
             <tbody className="text-center">
-              {data.map((i) => (
-                <tr className="border hover:bg-gray-100" key={i._id}>
+              {data.map((i, index) => (
+                <tr className="border hover:bg-gray-100" key={index}>
                   <td className="p-3">
                     <p>{i.title}</p>
                     <p>{i.desc}</p>
@@ -36,9 +81,13 @@ const Reservationbtn = ({ onClose, id }) => {
                   <td className="p-3">{i.maxPeople}</td>
                   <td className="p-3">TWD {i.price.toLocaleString()}</td>
                   <td className="p-3">
-                    {i.roomNumbers?.map((j) => (
-                      <label className="block" key={i._id}>
-                        <input type="checkbox" value={i._id} />
+                    {i.roomNumbers?.map((j, index) => (
+                      <label className="block" key={index}>
+                        <input
+                          type="checkbox"
+                          value={j._id}
+                          onChange={handleCheckBox}
+                        />
                         {j.number}
                       </label>
                     ))}
@@ -48,7 +97,15 @@ const Reservationbtn = ({ onClose, id }) => {
             </tbody>
           </table>
           <div className="flex justify-center">
-            <button className="p-3 border rounded-lg m-3">現在預訂</button>
+            <button
+              disabled={roomNumber.length == 0}
+              className={`p-3 border rounded-lg m-3 ${
+                roomNumber.length == 0 ? "cursor-not-allowed" : ""
+              }`}
+              onClick={handleClick}
+            >
+              現在預訂
+            </button>
           </div>
         </div>
       </div>
